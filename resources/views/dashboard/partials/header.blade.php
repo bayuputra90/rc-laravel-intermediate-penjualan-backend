01b538802c4d6e85ef61b790ffc1855561fdfd7a
bayuputra90 <bputra2212@gmail.com>
2023-05-04 16:34:07 +0700

    Add Laravel/UI for Auth

--- resources/views/dashboard/partials/header.blade.php
+++ resources/views/dashboard/partials/header.blade.php
@@ -12,10 +12,13 @@
 
                 <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
                     <img src="{{ asset('/') }}img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span
-                        class="text-dark">Charles Hall</span>
+                        class="text-dark">{{ auth()->user()->name }}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end">
-                    <a class="dropdown-item" href="#">Log out</a>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="dropdown-item">Log out</button>
+                    </form>
                 </div>
             </li>
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Administrator\UserController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SalesController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Administrator\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,26 +18,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route Dashboard
+Route::middleware('auth')->group(function(){
+    // Route Dashboard
 
-// Route Dashboard Home
-Route::get('/', [HomeController::class, 'index']);
+    // Route Dashboard Home
+    Route::get('/', [HomeController::class, 'index']);
 
-// Route Dashboard Product
-Route::get('product', [ProductController::class, 'index']);
-Route::get('product/create', [ProductController::class, 'create']);
-Route::post('product', [ProductController::class, 'store']);
-Route::get('product/{id}', [ProductController::class, 'show']);
-Route::get('product/{id}/edit', [ProductController::class, 'edit']);
-Route::put('product/{id}', [ProductController::class, 'update']);
-Route::delete('product/{id}', [ProductController::class, 'destroy']);
+    // Route Dashboard Product
+    Route::get('product', [ProductController::class, 'index']);
+    Route::get('product/create', [ProductController::class, 'create']);
+    Route::post('product', [ProductController::class, 'store']);
+    Route::get('product/{id}', [ProductController::class, 'show']);
+    Route::get('product/{id}/edit', [ProductController::class, 'edit']);
+    Route::put('product/{id}', [ProductController::class, 'update']);
+    Route::delete('product/{id}', [ProductController::class, 'destroy']);
 
-// Route Dashboard User
-Route::resource('user', UserController::class);
+    // Route Dashboard User
+    Route::resource('user', UserController::class);
 
-// Route dashboard Sales
-Route::get('sales', [SalesController::class, 'index'])->name('sales.index');
-Route::get('sales/{checkout}', [SalesController::class, 'show'])->name('sales.show');
-Route::put('sales/{checkout}', [SalesController::class, 'update'])->name('sales.update');
+    // Route dashboard Sales
+    Route::get('sales', [SalesController::class, 'index'])->name('sales.index');
+    Route::get('sales/{checkout}', [SalesController::class, 'show'])->name('sales.show');
+    Route::put('sales/{checkout}', [SalesController::class, 'update'])->name('sales.update');
+});
 
+Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
